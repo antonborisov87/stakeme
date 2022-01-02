348fe23206adbdbcab63af1b11895826ba0a7f19
willoyn <wloynmail@gmail.com>
2022-01-02 16:46:33 +0300

    contacts changes

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -5,8 +5,12 @@ const Contacts = () => {
     return (
         <div className={styles.mainContainer}>
             <div className={styles.title}>Contacts:</div>
-            <div className={styles.contactsContainer}>
+            <div className={styles.row}>
+                <div className={styles.title}>telegram: </div>
                 <a className={styles.link} href='https://t.me/antonbelarus87'>@antonbelarus87</a>
+            </div>
+            <div className={styles.row}>
+                <div className={styles.title}>discord: </div>
                 <a className={styles.link} href='https://discordapp.com/users/436540629049147392/'>RodgerVer#0492</a>
             </div>
         </div>
